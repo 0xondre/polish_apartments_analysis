85dba604aff8c547f3c211640e67710db2b53d76
Jedrzej Ciechanowski <jk.ciechanowski@gmail.com>
2025-05-05 20:15:58 +0200

    feat: initial regression model

--- regression.py
+++ regression.py
@@ -1,8 +1,12 @@
 import pandas as pd
-from sklearn.linear_model import LinearRegression
+import numpy as np
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
 
 # Data preparation for regression model
-buy_df = pd.read_csv("buy.csv").drop(["id"], axis=1)
+buy_df = pd.read_csv("buy.csv").drop(
+    ["id", "condition", "buildingMaterial", "type","latitude","longitude","ownership"], axis=1
+)
 
 binary_cols = [
     "hasParkingSpace",
@@ -14,16 +18,46 @@ binary_cols = [
 buy_df[binary_cols] = buy_df[binary_cols].fillna(0)
 buy_df[binary_cols] = buy_df[binary_cols].replace({"yes": 1, "no": 0})
 
-print(buy_df.head(10))
+num_cols = ["floor", "buildYear", "floorCount"]
+buy_df[num_cols] = buy_df[num_cols].fillna(buy_df[num_cols].median())
 
-# test_x = buy_df[buy_df['city']=='wroclaw'].loc[:, ~buy_df.columns.isin(['price','city'])]
-# test_y = buy_df[buy_df['city']=='wroclaw']['price']
+distance_cols = [
+    "schoolDistance",
+    "clinicDistance",
+    "postOfficeDistance",
+    "kindergartenDistance",
+    "restaurantDistance",
+    "collegeDistance",
+    "pharmacyDistance",
+]
 
-# for city in buy_df['city'].unique():
-#     if city == 'wroclaw':
-#         continue
-#     X = buy_df[buy_df['city']==city].loc[:, ~buy_df.columns.isin(['price','city'])]
-#     y = buy_df[buy_df['city']==city]['price']
-#     model = LinearRegression()
-#     model.fit(X,y)
-#     y_pred = model.predict(test_x)
+buy_df[distance_cols] = buy_df[distance_cols].fillna(buy_df[distance_cols].max())
+
+lower = buy_df["price"].quantile(0.01)
+upper = buy_df["price"].quantile(0.99)
+buy_df = buy_df[(buy_df["price"] >= lower) & (buy_df["price"] <= upper)]
+
+print(buy_df)
+print(buy_df.isnull().sum())
+
+
+# regression model
+x_test = buy_df[buy_df["city"] == "wroclaw"].drop(["price", "city"], axis=1)
+y_test = buy_df[buy_df["city"] == "wroclaw"]["price"]
+
+for city in buy_df['city'].unique():
+    if city == 'wroclaw':
+        continue
+    x = buy_df[buy_df['city']==city].drop(['price','city'], axis=1)
+    y = buy_df[buy_df['city']==city]['price']
+    model = RandomForestRegressor(n_estimators=10, max_depth=5, random_state=54)
+    model.fit(x, y)
+    y_pred = model.predict(x_test)
+    mae = mean_absolute_error(y_test, y_pred)
+    mse = mean_squared_error(y_test, y_pred)
+    r2 = r2_score(y_test, y_pred)
+    print(city)
+    print(f"MAE: {mae:.2f}")
+    print(f"MSE: {mse:.2f}")
+    print(f"R^2: {r2:.4f}")
+
